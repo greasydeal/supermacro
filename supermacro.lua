0ed4c91765b905c1cf86abf3dd000588dc63f79d
greasydeal <waylon_brown@hotmail.com>
2022-02-06 17:10:52 -0600

    Fixed text centering and added character limits.

--- supermacro.lua
+++ supermacro.lua
@@ -31,6 +31,7 @@ _addon.command = 'supermacro'
 _addon.commands = {'sm','smacro'}
 
 require('tables')
+require('strings')
 
 config = require('config')
 images = require('images')
@@ -57,8 +58,10 @@ do
    defaults.buttonTextRed = 0
    defaults.buttonTextGreen = 0
    defaults.buttonTextBlue = 0
-   defaults.buttonTextSize1 = 8
-   defaults.buttonTextSize2 = 10
+   defaults.buttonTextSize1 = 10
+   defaults.buttonTextCharLimit1 = 10
+   defaults.buttonTextSize2 = 12
+   defaults.buttonTextCharLimit2 = 10
    defaults.cursorAlpha = 255
    defaults.cursorColorRed = 100
    defaults.cursorColorGreen = 0
@@ -69,12 +72,13 @@ do
    defaults.mCursorColorBlue = 0
    defaults.spacingX = 5
    defaults.spacingY = 5
-   defaults.posX = 450
-   defaults.posY = 550
+   defaults.posX = 400
+   defaults.posY = 650
    defaults.cursorBlinkTime = 1
-   defaults.pageChangeKey = 'tab'
 
    settings = config.load(defaults)
+
+   settings:save('all')
 
    menuOpen = false
    modMode = false
@@ -330,6 +334,33 @@ function GetButtonData(bar)
 
 end
 
+function GetStringWidth(str, fontSize)
+   local letterWidthTable = {{" ",27.797},{"!",33.313},{"\"",47.422},{"#",55.625},{"$",55.625},{"%",88.922},{"&",72.219},{"'",23.781},{"(",33.313},{")",33.313},{"*",38.922},{"+",58.406},{",",27.797},{"-",33.313},{".",27.797},{"/",27.797},{"0",55.625},{"1",55.625},{"2",55.625},{"3",55.625},{"4",55.625},{"5",55.625},{"6",55.625},{"7",55.625},{"8",55.625},{"9",55.625},{":",33.313},{";",33.313},{"<",58.406},{"=",58.406},{">",58.406},{"?",61.094},{"@",97.516},{"A",72.219},{"B",72.219},{"C",72.219},{"D",72.219},{"E",66.703},{"F",61.094},{"G",77.797},{"H",72.219},{"I",27.797},{"J",55.625},{"K",72.219},{"L",61.094},{"M",83.313},{"N",72.219},{"O",77.797},{"P",66.703},{"Q",77.797},{"R",72.219},{"S",66.703},{"T",61.094},{"U",72.219},{"V",66.703},{"W",94.391},{"X",66.703},{"Y",66.703},{"Z",61.094},{"{",33.313},{"\\",27.797},{"}",33.313},{"^",58.406},{"_",55.625},{"`",33.313},{"a",55.625},{"b",61.094},{"c",55.625},{"d",61.094},{"e",55.625},{"f",33.313},{"g",61.094},{"h",61.094},{"i",27.797},{"j",27.797},{"k",55.625},{"l",27.797},{"m",88.922},{"n",61.094},{"o",61.094},{"p",61.094},{"q",61.094},{"r",38.922},{"s",55.625},{"t",33.313},{"u",61.094},{"v",55.625},{"w",77.797},{"x",55.625},{"y",55.625},{"z",50},{"{",38.922},{"|",27.984},{"}",38.922},{"~",58.406},{"_median",55.625}}
+   local kernModifier = {{" A",-3.719},{" Y",-1.813},{"11",-5.531},{"A ",-3.719},{"AT",-7.422},{"AV",-7.422},{"AW",-5.516},{"AY",-9.172},{"Av",-3.719},{"Aw",-1.813},{"Ay",-3.719},{"F,",-11.094},{"F.",-11.094},{"FA",-5.516},{"L ",-1.829},{"LT",-7.438},{"LV",-7.422},{"LW",-5.532},{"LY",-9.188},{"Ly",-3.719},{"P ",-1.813},{"P,",-12.906},{"P.",-12.906},{"PA",-7.422},{"RV",-1.813},{"RW",-1.813},{"RY",-3.703},{"T,",-11.094},{"T-",-5.532},{"T.",-11.094},{"T:",-11.095},{"T;",-11.095},{"TA",-7.422},{"TO",-1.828},{"Ta",-7.438},{"Tc",-7.438},{"Te",-7.438},{"Ti",-1.829},{"To",-7.438},{"Tr",-5.532},{"Ts",-7.438},{"Tu",-7.438},{"Tw",-7.438},{"Ty",-7.438},{"V,",-9.188},{"V-",-5.532},{"V.",-9.188},{"V:",-5.532},{"V;",-5.532},{"VA",-7.422},{"Va",-5.531},{"Ve",-5.531},{"Vi",-1.813},{"Vo",-7.422},{"Vr",-5.516},{"Vu",-3.719},{"Vy",-3.719},{"W,",-5.532},{"W-",-2.017},{"W.",-5.532},{"W:",-1.813},{"W;",-1.813},{"WA",-5.516},{"Wa",-3.719},{"We",-1.813},{"Wi",-0.891},{"Wo",-1.813},{"Wr",-1.813},{"Wu",-1.813},{"Wy",-1.813},{"Y ",-1.813},{"Y,",-11.094},{"Y-",-5.532},{"Y.",-11.094},{"Y:",-7.438},{"Y;",-7.438},{"YA",-9.172},{"Ya",-5.531},{"Ye",-5.531},{"Yi",-3.719},{"Yo",-7.422},{"Yp",-5.531},{"Yq",-7.422},{"Yu",-5.531},{"Yv",-5.531},{"r,",-5.531},{"r.",-5.531},{"v,",-7.438},{"v.",-7.438},{"w,",-3.735},{"w.",-3.735},{"y,",-7.438},{"y.",-7.438}}
+   local stringWidth = 0
+   local charStepCount = 0
+
+   str = str:trim()
+   for c in str:it() do
+      charStepCount = charStepCount + 1
+      for t in pairs(letterWidthTable) do
+         if letterWidthTable[t][1] == c then
+            stringWidth = stringWidth + letterWidthTable[t][2]
+            for k in pairs(kernModifier) do
+               if kernModifier[k][1] == (c.. str[charStepCount + 1]) then
+                  stringWidth = stringWidth + kernModifier[k][2]
+               end
+            end
+         end
+      end
+   end
+   return ((stringWidth * 1.25) / (100/fontSize))
+end
+
+function TrimString(str, length)
+   return str:slice(1, length)
+end
+
 ---------------------------------------------------------------
 -- Parses profile .xml files for bar data
 ---------------------------------------------------------------
@@ -398,10 +429,10 @@ function BuildActionBar()
 
    barList = ParseBarData()
 
-   buttonX = buttonXStart
-   buttonY = buttonYStart
-   buttonSizeX = settings.buttonSizeX
-   buttonSizeY = settings.buttonSizeY
+   local buttonX = buttonXStart
+   local buttonY = buttonYStart
+   local buttonSizeX = settings.buttonSizeX
+   local buttonSizeY = settings.buttonSizeY
 
    ---------------------------------------------------------------------------------------------
    -- Itterates though bar and button counts creating button objects and stores them in a table sturcture as follows:
@@ -409,62 +440,77 @@ function BuildActionBar()
    ---------------------------------------------------------------------------------------------
    for i, bar in pairs(barList) do
 
-      macroButtonList = T{} --meant to be a list of buttonContainers
-      barData = GetButtonData(bar)
+      local macroButtonList = T{} --meant to be a list of buttonContainers
+      local barData = GetButtonData(bar)
 
       for j=1,buttonCount do
 
-         lineYOffset = settings.buttonTextSize1 / (72/96)
+         local lineYOffset = settings.buttonTextSize1 / (72/96)
 
-         buttonData = T{} -- Text Line 1, Text Line 2, Command
-         buttonContainer = T{} -- buttonUI, buttonData
-         buttonUI = T{} -- buttonBg, buttonText_1
+         local buttonData = T{} -- Text Line 1, Text Line 2, Command
+         local buttonContainer = T{} -- buttonUI, buttonData
+         local buttonUI = T{} -- buttonBg, buttonText_1
+
+         -- Clean button data
+         if barData[j][1] == '#empty#' then
+            barData[j][1] = ''
+         end
+         if barData[j][2] == '#empty#' then
+            barData[j][2] = ''
+         end
+         if barData[j][5] == '#empty#' then
+            barData[j][5] = barData[j][1]
+         end
+         if barData[j][6] == '#empty#' then
+            barData[j][6] = barData[j][2]
+         end
 
          -- Add data to buttonData table
          buttonData:append(barData[j][1]) -- Text Line 1
          buttonData:append(barData[j][2]) -- Text Line 2
          buttonData:append(barData[j][3]) -- command
          buttonData:append(barData[j][4]) -- modified command
-         buttonData:append(barData[j][5]) -- Button type
+         buttonData:append(barData[j][5]) -- modified text line 1
+         buttonData:append(barData[j][6]) -- modified text line 2
 
          -- Create button background
-         buttonBg = images.new()
+         local buttonBg = images.new()
          buttonBg:color(settings.buttonColorRed,settings.buttonColorGreen,settings.buttonColorBlue)
          buttonBg:alpha(settings.buttonAlpha)
          buttonBg:size(settings.buttonSizeX,settings.buttonSizeY)
          buttonBg:draggable(false)
          buttonBg:pos(buttonX, buttonY)
-         buttonExtentsX, ButtonExtentsY = buttonBg:get_extents()
+         buttonExtentsX, buttonExtentsY = buttonBg:get_extents()
 
          -- Create button text line 1
-         buttonText_1 = texts.new(buttonData[1])
+         local buttonText_1 = texts.new(TrimString(buttonData[1], settings.buttonTextCharLimit1))
          buttonText_1:bg_visible(false)
          buttonText_1:size(settings.buttonTextSize1)
          buttonText_1:stroke_alpha(100)
          buttonText_1.draggable = false
-         textExtentsX_1, textExtentsY_1 = buttonText_1:extents()
+         buttonText_1_width = GetStringWidth(buttonText_1:text(), settings.buttonTextSize1)
          --buttonText_1:pos(buttonX, buttonY)
 
          -- Create button text line 2
          if not (buttonData[2] == nil) then
-            buttonText_2 = texts.new(buttonData[2])
+            buttonText_2 = texts.new(TrimString(buttonData[2], settings.buttonTextCharLimit2))
             buttonText_2:bg_visible(false)
             buttonText_2:size(settings.buttonTextSize2)
             buttonText_2:stroke_alpha(100)
             buttonText_2.draggable = false
-            textExtentsX_2, textExtentsY_2 = buttonText_2:extents()
+            buttonText_2_width = GetStringWidth(buttonText_2:text(), settings.buttonTextSize2)
             --buttonText_2:pos(buttonX, buttonY + lineYOffset)
          end
 
          -- Center text lines horizontally and vertially
-         textOffsetX_1 = ((buttonExtentsX - buttonX) - textExtentsX_1) / 4
-         textOffsetY_1 = ((ButtonExtentsY - buttonY) - (textExtentsY_1 + textExtentsY_2)) / 4
+         local textOffsetX_1 = ((buttonExtentsX - buttonX) - buttonText_1_width) / 2
+         local textOffsetY_1 = ( (buttonExtentsY - buttonY) - ( (settings.buttonTextSize1 / (72/96) ) + ( settings.buttonTextSize2 / (72/96) ) ) ) /2 
          buttonText_1:pos(buttonX + textOffsetX_1, buttonY + textOffsetY_1)
 
          
          if not (buttonData[2] == nil) then
-            textOffsetX_2 = ((buttonExtentsX - buttonX) - textExtentsX_2) / 4
-            textOffsetY_2 = textOffsetY_1 + lineYOffset
+            local textOffsetX_2 = ((buttonExtentsX - buttonX) - buttonText_2_width) / 2
+            local textOffsetY_2 = textOffsetY_1 + lineYOffset
             buttonText_2:pos(buttonX + textOffsetX_2, buttonY + textOffsetY_2)
          end
 
@@ -614,9 +660,15 @@ function FontToPixel(fSize)
 end
 
 windower.register_event('load', function()
-   GetBarsXML()
-   BuildActionBar()
-   ChangeCursorColor()
+   local run = true
+
+   if run == true then
+      GetBarsXML()
+      BuildActionBar()
+      ChangeCursorColor()
+      else
+         TestFunction()
+   end
 end)
 
 windower.register_event('prerender', function()   
@@ -662,3 +714,7 @@ windower.register_event('keyboard', function(dik, down, flags, blocked)
       end
    end
 end)
+
+function TestFunction()
+
+end
